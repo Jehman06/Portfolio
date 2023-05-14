540cb236718e1b05675f4f721ada12a4049a8a8c
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-05-13 20:19:37 -0700

    Created the portfolio template

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        title: 'REST-Rant',
+        subtitle: 'MERN Stack',
+        description: 'A MERN Stack application that allows to create, update, delete, review and comment restaurants and businesses.',
+        image: "",
+        link: 'https://github.com/Jehman06/project-REST-rant.git'
+    },
+    {
+        title: 'React-A-Mole',
+        subtitle: 'React',
+        description: 'Whac-A-Mole mini-game.',
+        image: "",
+        link: 'https://github.com/Jehman06/React-A-Mole.git'
+    },
+    {
+        title: "Weather App",
+        subtitle: "React",
+        description: "A single-page web application that uses the OpenWeather API. The 2 radio buttons allow to convert Imperial to Metric, causing a re-render with the correct information.",
+        image: "./weather-app-gif.gif",
+        link: "https://jehman06.github.io/weather-app/"
+    }
+]
+
+export const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Python',
+    'React',
+    'Express',
+    'MongoDB',
+    'Mongoose',
+    'Bootstrap',
+    'Tailwind',
+    'Material UI',
+    'REST'
+]
